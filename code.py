610c87135a12b7826fe49a93e855d7d32a1eb575
vishnushaji26 <vishnushaji2602@gmail.com>
2024-07-06 19:43:56 +0530

    First Commit
    First Date: 07/06/2024

---
+++ code.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import prophet as fbp
+from sklearn.metrics import mean_squared_error, mean_absolute_error
+# Use fivethirtyeight plot style
+plt.style.use('fivethirtyeight')
